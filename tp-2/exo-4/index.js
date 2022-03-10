3caca8d20436d4c310997e8494432e2ec7bde649
Ratsimandresy Rabeson <ricorabesoa@gmail.com>
2022-03-11 00:14:27 +0100

    feat(tp2): exo 4 tp-2

--- tp-2/exo-4/index.js
+++ tp-2/exo-4/index.js
@@ -1,1 +1,37 @@
-console.log("exo 4");
+const square = document.querySelector(".square");
+const body = document.getElementsByTagName("body")[0];
+
+let X = square.getBoundingClientRect().left;
+let Y = square.getBoundingClientRect().top;
+
+body.style.position = "relative";
+square.style.position = "absolute";
+square.style.width = "50px";
+square.style.height = "50px";
+square.style.backgroundColor = "pink";
+
+document.onkeydown = (e) => {
+  let key = e.key;
+
+  switch (key) {
+    case "ArrowUp":
+      console.log("up");
+      square.style.top = `${(Y -= 20)}px`;
+      break;
+    case "ArrowDown":
+      console.log("down");
+      square.style.top = `${(Y += 20)}px`;
+      break;
+    case "ArrowRight":
+      console.log("right");
+      square.style.left = `${(X += 20)}px`;
+      break;
+    case "ArrowLeft":
+      console.log("left");
+      square.style.left = `${(X -= 20)}px`;
+      break;
+
+    default:
+      break;
+  }
+};
