15acc2ebffd019762b7b465882e174d5c7d4e59a
Ratsimandresy Rabeson <ricorabesoa@gmail.com>
2022-03-10 21:38:48 +0100

    feat(tp1-tp2): exo 7 tp-1

--- tp-1/exo-7/index.js
+++ tp-1/exo-7/index.js
@@ -1,1 +1,7 @@
-console.log("exo 7");
+const bodyBgColor =
+  document.getElementsByTagName("body")[0].style.backgroundColor;
+const bodyWidth = document.getElementsByTagName("body")[0].style.width;
+
+console.log(
+  `The body's background-color is : ${bodyBgColor}, and the body's width is : ${bodyWidth}`
+);
